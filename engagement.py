e210dc955fc6b4fed2a48e3409fd6d106129c93f
JasonAhn <jasonja.ahn@gmail.com>
2022-01-08 00:51:20 -0800

    Insert pwc logo

--- engagement.py
+++ engagement.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog, Text
+from pil import ImageTk, Image
+
 import os
 
 # create GUI window
@@ -9,6 +11,9 @@ canvas = tk.Canvas(root, height = 500, width = 400, bg = "grey")
 canvas.pack()
 #canvas.title("PwC Engagement")
 
+path = "pwc_logo.png"
+img = ImageTk.PhotoImage(Image.open(path))
+
 frame = tk.Frame(root, bg = "lightgrey")
 frame.place(relwidth = 0.8, relheight = 0.8, rely = 0.1, relx = 0.1)
 
@@ -17,7 +22,7 @@ entry1 = tk.Entry(root)
 
 def textBoxCreate():
     canvas.create_window(200, 140, window = entry1)
-    
+
 # button to quit the program
 quitButton = tk.Button(frame, text = "QUIT", fg = "red", command = quit)
 quitButton.pack(side = tk.BOTTOM, padx = 10, pady = 10)
