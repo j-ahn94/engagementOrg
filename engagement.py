35191e8af69a27e73e1593ce79535f8a94d0f8eb
JasonAhn <jasonja.ahn@gmail.com>
2022-01-08 14:44:13 -0800

    re-size pwc logo image

--- engagement.py
+++ engagement.py
@@ -15,12 +15,25 @@ canvas.pack()
 #path = "pwc_logo.png"
 #img = ImageTk.PhotoImage(Image.open(path))
 
-img = ImageTk.PhotoImage(Image.open("pwc_logo.png"))
-canvas.create_image(20, 20, anchor= tk.NW, image = img)
+img = (Image.open("pwc_logo.png"))
+#img = ImageTk.PhotoImage(Image.open("pwc_logo.png"))
+
+#canvas.create_image(0.1, 0.1, anchor= tk.W, image = img)
 
 
 frame = tk.Frame(root, bg = "lightgrey")
 frame.place(relwidth = 0.8, relheight = 0.8, rely = 0.1, relx = 0.1)
+
+resized_image = img.resize((80, 50), Image.ANTIALIAS)
+new_image = ImageTk.PhotoImage(resized_image)
+
+#frame.create_image(10, 10, anchor=tk.NW, image=new_image)
+
+#Display PwC logo image
+label = tk.Label(frame, image = new_image)
+label.image = new_image
+label.place(x = 1, y = 1)
+
 
 # create text field when clicking a 'Add' button
 entry1 = tk.Entry(root)
@@ -35,7 +48,6 @@ quitButton.pack(side = tk.BOTTOM, padx = 10, pady = 10)
 # button to add engagement
 addButton = tk.Button(frame, text = "CREATE", fg = "red", command = textBoxCreate)
 addButton.pack(side = tk.BOTTOM)
-
 
 
 def write_slogan():
